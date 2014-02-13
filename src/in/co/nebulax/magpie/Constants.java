82cedd987bbaaf637c4a7da174d217dea2c071e4
Akshay Deshraj <akshay30nov@gmail.com>
2014-02-13 14:06:05 +0530

    Add Constants class for url and JSON node names

---
+++ src/in/co/nebulax/magpie/Constants.java
@@ -0,0 +1,13 @@
+package in.co.nebulax.magpie;
+
+public class Constants {
+
+	// url to get data JSON
+	public static String url = "";
+	
+	//JSON Node Names
+	public static String TAG_DATA = "data";
+	public static String TAG_DEVICE = "device";
+	public static String TAG_STATUS = "status";
+	public static String TAG_NAME = "name";
+}

b88c56b7fdc4772aec8afa3036be02382ab1bacb
Akshay Deshraj <akshay30nov@gmail.com>
2014-02-24 22:35:21 +0530

    Add url for post request

--- src/in/co/nebulax/magpie/Constants.java
+++ src/in/co/nebulax/magpie/Constants.java
@@ -3,7 +3,8 @@ package in.co.nebulax.magpie;
 public class Constants {
 
 	// url to get data JSON
-	public static String url = "http://10.0.2.2:1111/all";
+	public static String urlGet = "http://10.0.2.2:1111/all";
+	public static String urlPost = "http://10.0.2.2:1111/change";
 	
 	//JSON Node Names
 	public static String TAG_DATA = "data";
